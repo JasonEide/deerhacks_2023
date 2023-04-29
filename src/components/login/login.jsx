2cf68f4e7a34ca6ce8855d09dc6ecd3a9370f2de
jason <jason.eide@mail.utoronto.ca>
2023-04-28 23:03:47 -0400

    initial setup and commit

---
+++ src/components/login/login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './login.module.css';
+
+export default function Login() {
+    return (
+        <div>
+            Hello
+        </div>
+    )
+
+}
---
+++ src/components/register/register.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './register.module.css';
+
+export default function Register() {
+    return (
+        <div>
+            Hello
+        </div>
+    )
+
+}
