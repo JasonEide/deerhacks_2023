bfd2141f8628df9e4f2b2877daaec865f7a0640f
Damon Ma <damondma@gmail.com>
2023-04-29 13:54:14 -0400

    Update homepage.jsx

--- src/components/homepage/homepage.jsx
+++ src/components/homepage/homepage.jsx
@@ -33,14 +33,27 @@ export default function Homepage({data}) {
         if(is_logged){
             curr_user = (JSON.parse(localStorage.getItem("curr_user")));
         }
-        updateExp();
+        let gainedExp = getExp(originalMsg, msg);
+
+
+
+        updateExp(gainedExp, is_logged, curr_user);
     };
 
     async function updateExp(val, is_logged, curr_user) {
         if (is_logged) {
             const user_ref = doc(db, "users", curr_user.id);
             let temp_exp = curr_user.exp + val;
+<<<<<<< Updated upstream
 
+=======
+            let temp_lvl = curr_user.lvl;
+            if (temp_exp >= temp_lvl**2){
+                temp_exp = temp_exp - temp_lvl**2;
+                temp_lvl++;
+            } 
+            
+>>>>>>> Stashed changes
             await updateDoc(user_ref, {exp: temp_exp, lvl: temp_lvl});
         }
     }
@@ -118,4 +131,41 @@ export default function Homepage({data}) {
             </div>
         </div>
     );
+}
+
+
+
+
+/**
+ * Given the original grammatically-correct sentence and the user submission, determine how many Exp points 
+ * the user will receive from editing a sentence
+ * @param {*} correctText - The original generated sentence
+ * @param {*} userText - The user-submitted sentence
+ */
+function getExp(correctText, userText){
+    const correctWords = correctText.split(" ");
+    const userWords = userText.split(" ");
+    //user didn't follow the rules and added/removed words, give 0xp.
+    if(correctWords.length === userWords.length){
+        return 0;
+    }
+    let numCorrect = 0;
+    let numWrong = 0;
+
+    //check how many words in the sentence they got right and how many they got wrong
+    for(let i = 0; i < correctWords.length; i++){
+        if(correctWords[i] === userWords[i]){
+            numCorrect++;
+        }else{
+            numWrong++;
+        }
+    }
+
+    //exp = correct grammar per word in the sentence - incorrect grammar per word in sentence (0 Exp if more wrong than right)
+    if(numCorrect < numWrong){
+        return 0;
+    }else{
+        return numCorrect - numWrong;
+    }
+
 }
